4f9f20fba5d8f75fed6d138b6c2918db56ac2bb1
jose.va18 <jose.va18@5cc8ff15-4a09-fb00-8592-0cb6abb8ad67>
2011-11-04 14:00:21 +0000

    añadiendo libreria de slam graph

---
+++ trunk/aislib/graph/loadEdges3d.d
@@ -0,0 +1,15 @@
+loadEdges3d.o: loadEdges3d.cpp loadEdges3d.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/transformation.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/rotation_matrix.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/matrix_n.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/vector_n.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/stuff/array_allocator.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/stuff/macros.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/vector_n.hpp \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/matrix_n.hpp \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/rotation_matrix.hpp \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/angle.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/quaternion.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/quaternion.hpp \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/axis_angle.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/axis_angle.hpp
---
+++ trunk/aislib/graph_optimizer_hogman/hogman2d.d
@@ -0,0 +1,43 @@
+hogman2d.o: hogman2d.cpp \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/graph_optimizer/graph_optimizer2d_aux.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/transformation.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/rotation_matrix.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/matrix_n.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/vector_n.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/stuff/array_allocator.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/stuff/macros.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/vector_n.hpp \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/matrix_n.hpp \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/rotation_matrix.hpp \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/angle.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/quaternion.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/quaternion.hpp \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/axis_angle.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/axis_angle.hpp \
+ graph_optimizer2d_chol.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/graph_optimizer/graph_optimizer2d.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/graph_optimizer/graph_optimizer.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/graph/posegraph.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/graph/graph.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/graph/dijkstra.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/graph/posegraph.hpp \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/graph_optimizer/graph_optimizer.hpp \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/graph/posegraph2d.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/graph/posegraph.h \
+ graph_optimizer_chol.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/graph_optimizer/graph_optimizer.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/math/transformation.h \
+ graph_optimizer_chol.hpp \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/stuff/os_specific.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/stuff/color_macros.h \
+ csparse_helper.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/EXTERNAL/csparse/cs.h \
+ graph_optimizer2d_hchol.h graph_optimizer_hchol.h \
+ graph_optimizer_hchol.hpp \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/stuff/macros.h \
+ graph_optimizer_hchol_aux.hpp graph_optimizer_hchol_batch.hpp \
+ graph_optimizer_hchol_incremental.hpp \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/graph_optimizer/graph_optimizer2d.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/stuff/filesys_tools.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/stuff/string_tools.h \
+ /home/jose/Escritorio/hogman/hog-man/trunk/aislib/stuff/runtime_error.h
